cf1b0476ebcb60e95db51555d33090e7dd1c91e0
g0nnz0 <bonzai_rock@hotmail.com>
2025-06-01 13:47:52 -0300

    Gestor de pacientes: Esqueleto

---
+++ Gestores.py
@@ -0,0 +1,23 @@
+from clasesModelo import Paciente
+
+class GestorPacientes:
+    def __init__(self, nombreArchivoPacientes: str):
+        self.listaDePacientes: list[Paciente] = []
+        self.nombreArchivoPacientes = nombreArchivoPacientes
+
+    def cargarListaPacientes(self):
+        pass
+
+
+    def guardarListaPacientes(self):
+        pass
+
+
+    def agregarPaciente(self):
+        pass
+
+    def listarPacientes(self):
+        pass
+
+    def modificarPaciente(self):
+        pass
