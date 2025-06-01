476dc5b4a50b61f4c50ec75cf47b6c73302c3e63
g0nnz0 <bonzai_rock@hotmail.com>
2025-06-01 14:38:26 -0300

    GestorPacientes: Persistencia implementada

--- Gestores.py
+++ Gestores.py
@@ -1,16 +1,34 @@
+import pickle
+import os
 from clasesModelo import Paciente
+
 
 class GestorPacientes:
     def __init__(self, nombreArchivoPacientes: str):
-        self.listaDePacientes: list[Paciente] = []
         self.nombreArchivoPacientes = nombreArchivoPacientes
+        self.listaDePacientes = self._cargarListaPacientes()
+        
 
-    def cargarListaPacientes(self):
-        pass
+    def _cargarListaPacientes(self):
+        try:
+            if not os.path.exists(self.nombreArchivoPacientes):
+                print(f"No se encontró el archivo {self.nombreArchivoPacientes}. Se inicializa vacio")
+                return []
+            with open(self.nombreArchivoPacientes, "rb") as f:
+                return pickle.load(f)
+        except EOFError:
+            print(f"El archivo {self.nombreArchivoPacientes} se inicializa vacio.")
+            return []
+        except Exception as e:
+            print(f"Ocurrio un error inesperado: {e}")
 
 
-    def guardarListaPacientes(self):
-        pass
+    def _guardarListaPacientes(self):
+        try:
+            with open(self.nombreArchivoPacientes, "wb") as f:
+                pickle.dump(self.listaDePacientes, f)
+        except Exception as e:
+            print(f"Ocurrio un error inesperado: {e}")
 
 
     def agregarPaciente(self):
